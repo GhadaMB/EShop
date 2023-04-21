4d926f9c4f3819c57ea340f4b0a55d55ffabaa74
Ghada <ghadabaroud996@gmail.com>
2023-04-21 14:46:24 +0300

    add category

--- resources/views/layouts/inc/sidebar.blade.php
+++ resources/views/layouts/inc/sidebar.blade.php
@@ -184,16 +184,16 @@
       <li class="nav-heading">Pages</li>
 
       <li class="nav-item">
-        <a class="nav-link collapsed" href="users-profile.html">
+        <a class="nav-link collapsed" href="{{ url('categories') }}">
           <i class="bi bi-person"></i>
-          <span>Profile</span>
+          <span>Categories</span>
         </a>
       </li><!-- End Profile Page Nav -->
 
       <li class="nav-item">
-        <a class="nav-link collapsed" href="pages-faq.html">
+        <a class="nav-link collapsed" href="{{ url('add-category') }}">
           <i class="bi bi-question-circle"></i>
-          <span>F.A.Q</span>
+          <span>Add Category</span>
         </a>
       </li><!-- End F.A.Q Page Nav -->
 
