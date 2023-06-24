6da69c1f0da09b29b6040e5e996a477875851691
Ghada <ghadabaroud996@gmail.com>
2023-06-24 18:41:39 +0300

    frontend slider & navbar

---
+++ resources/views/frontend/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.front')
+
+@section('title')
+    welcome to E-shop
+@endsection
+
+@section('content')
+    @include('layouts.inc.front.slider')
+    <h1>Welcome</h1>
+@endsection
