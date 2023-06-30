9c6833b74f119c3ddcc279ffb0f72b9ed0b62da5
Ghada <ghadabaroud996@gmail.com>
2023-06-30 20:21:14 +0300

    add category page

--- app/Http/Controllers/Frontend/FrontendController.php
+++ app/Http/Controllers/Frontend/FrontendController.php
@@ -5,11 +5,19 @@ namespace App\Http\Controllers\Frontend;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Product;
+use App\Models\Category;
 
 class FrontendController extends Controller
 {
     public function index() {
-        $featured_products = Product::where('trending','1')->take(5)->get();
-        return view('frontend.index', compact('featured_products'));
+        $featured_products = Product::where('trending','1')->take(10)->get();
+        $trending_categories = Category::where('popular','1')->take(10)->get();
+        return view('frontend.index', compact('featured_products', 'trending_categories'));
+    }
+
+    public function category() {
+
+        $category = Category::where('status','0')->get();
+        return view('frontend.category', compact('category'));
     }
 }
--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -28,11 +28,53 @@
             </div>
         </div>
     </div>
+
+    <div class="py-5">
+        <div class="container">
+            <div class="row">
+                <h2>Trending Categories</h2>
+                <div class="owl-carousel owl-theme trending-carousel">
+                    @foreach($trending_categories as $cate)
+                        <div class="item">
+                            <div class="card">
+                                <img src="{{ asset('assets/uploads/category/'. $cate->image) }}" alt="Product Image">
+                                <div class="card-body">
+                                    <h5>{{ $cate->name }}</h5>
+                                    <p>{{ $cate->description }}</p>
+                                </div>
+                            </div>
+                        </div>
+                    @endforeach
+                </div>
+
+            </div>
+        </div>
+    </div>
 @endsection
 
 @section('scripts')
     <script>
+        // Carousel for products
         $('.featured-carousel').owlCarousel({
+            loop:true,
+            margin:10,
+            nav:true,
+            dots:false,
+            responsive:{
+                0:{
+                    items:1
+                },
+                600:{
+                    items:3
+                },
+                1000:{
+                    items:4
+                }
+            }
+        })
+
+        // Carousel for categories
+        $('.trending-carousel').owlCarousel({
             loop:true,
             margin:10,
             nav:true,
