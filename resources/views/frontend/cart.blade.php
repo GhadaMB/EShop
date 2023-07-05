e2ee438975a41cc8d18aa52f933695c6cbafb20c
Ghada <ghadabaroud996@gmail.com>
2023-07-06 00:50:19 +0300

    checkout page & out of stock

--- resources/views/frontend/cart.blade.php
+++ resources/views/frontend/cart.blade.php
@@ -38,24 +38,28 @@
                             </div>
                             <div class="col-md-3 my-auto">
                                 <input type="hidden" class="prod_id" value="{{ $item->prod_id }}">
-                                <label for="Quantity">Quantity</label>
-                                <div class="input-group text-center mb-3" style="width:130px;">
-                                    <button class="input-group-text changeQuantity decrement-btn">-</button>
-                                    <input type="text" name="quantity" value="{{ $item->prod_qty }}" class="form-control qty-input"/>
-                                    <button class="input-group-text changeQuantity increment-btn">+</button>
-                                </div>
+                                @if( $item->products->qty > $item->prod_qty )
+                                    <label for="Quantity">Quantity</label>
+                                    <div class="input-group text-center mb-3" style="width:130px;">
+                                        <button class="input-group-text changeQuantity decrement-btn">-</button>
+                                        <input type="text" name="quantity" value="{{ $item->prod_qty }}" class="form-control qty-input"/>
+                                        <button class="input-group-text changeQuantity increment-btn">+</button>
+                                    </div>
+                                    @php $total += $item->products->selling_price * $item->prod_qty; @endphp
+                                @else
+                                    <h6>Out Of Stock</h6>
+                                @endif
                             </div>
                             <div class="col-md-2 my-auto">
                                 <br>
                                 <button class="btn btn-danger delete-cart-item"><i class="fa fa-trash"></i> Remove</button>
                             </div>
                         </div>
-                        @php $total += $item->products->selling_price * $item->prod_qty; @endphp
                     @endforeach
                 </div>
                 <div class="card-footer">
                     <h6>Total Price : Rs {{ $total }}
-                        <button class="btn btn-outline-success float-end">Proceed To Checkout</button>
+                        <a href="{{ url('checkout') }}" class="btn btn-outline-success float-end">Proceed To Checkout</a>
                     </h6>
                 </div>
             </div>
